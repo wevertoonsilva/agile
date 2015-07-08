f1cf632a70415dba5d6e4c4932374a09cbedebcd
Weverton Silva <mail.weverton@gmail.com>
2015-07-08 16:29:27 -0300

    Criação do Controller da página principal

---
+++ src/main/java/com/intercasa/agile/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.intercasa.agile.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author weverton
+ * @date 08/07/2015 @time 16:27:50
+ */
+@Controller
+public class HomeController {
+    
+}
